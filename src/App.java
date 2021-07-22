8a6d865c14fe931b4e0aa94b90b261e4065bc9b1
HelixFix <jerome.rinner@gmail.com>
2021-07-22 11:27:26 +0200

    :heavy_plus_sign: Add fxml for use with scene builder

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import controller.MainController;
@@ -16,7 +18,9 @@ public class App extends Application{
         MainController controller = new MainController(primaryStage, model);
         MainView view = new MainView(model, controller);
 
-        Scene scene = new Scene(view, 500, 500);
+
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene scene = new Scene(root, 500, 500);
         primaryStage.setTitle("Sample JavaFx MVC APP");
         primaryStage.setScene(scene);
         primaryStage.show();
